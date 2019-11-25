238a615939017f76d9dd16ff474e8fe153fee961
Antonio Maiocchi <antonio.maiocchi12@gmail.com>
2019-11-25 18:00:31 +0100

    day 1

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,26 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
-    @letters = ('A'..'Z').to_a.sample(rand(1..15))
+    @letters = ('A'..'Z').to_a.sample(rand(9..15))
   end
 
   def score
+    @score = params[:score]
+    @letters = params[:letters]
+    if !@score.chars.all? { |place| @letters.include?(place) }
+      @answer = "Sorry but #{@score} can't be built out of #{@letters}"
+    elsif !english_word?(@score)
+      @answer = "Sorry but #{@score} doesn't seem to be a valid English word"
+    else
+      @answer = "Congratulations! #{@score} is a valid English word!"
+    end
   end
+
+  def english_word?(answer)
+    response = open("https://wagon-dictionary.herokuapp.com/#{answer}")
+    json = JSON.parse(response.read)
+    json['found']
+ end
 end
